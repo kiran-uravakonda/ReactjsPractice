02913f030d81c2d7059b58b19100c458acf16877
Kiran-urakonda <kiran123556789@gmail.com>
2023-12-04 19:30:10 +0530

    maincode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
+var isLogin=false;
+let isAdmin=true
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <Header></Header>
+    
+     { 
+      isAdmin &&  <Admin/>
+     }
     </div>
   );
+}
+
+function Header(){
+  return(
+    <div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        {
+          isLogin?<li>Logout</li>:<li>Login</li>
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Admin(){
+  return(
+    <h1>Welcome to the Admin page</h1>
+  )
 }
 
 export default App;
