ae77028bf4794173c096245954e7179cc70f36ee
Kiran-urakonda <kiran123556789@gmail.com>
2023-12-04 22:50:39 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React,{useState} from 'react'
-
+import React ,{useState,useEffect} from 'react'
 function App() {
-  let[isLogin,setLogin]=useState(false)
+  let [update,setUpdate]=useState(0)
+ useEffect(()=>{
+  console.log("use effect")
+ },[update])
+
+//  useEffect(()=>{
+//   console.log("use effect")
+//  },[])
+
+//  useEffect(()=>{
+//   console.log("use effect")
+//  })
+ 
+function Increase(){
+     setUpdate(update+1)
+}
+
   return (
     <div>
-      {isLogin ? (
-        <div>
-          <h1>Welcome Back</h1>
-          <button onClick={Logout}>logout</button>
-        </div>
-      ) : (
-        <div>
-          <h1>Please Login</h1>
-          <button onClick={Login}>login</button>
-        </div>
-      )}
+      {console.log("functional compont")}
+     <h1>Functional Component</h1>
+     <h1>value is:{update}</h1>
+     <button onClick={Increase}>click</button>
     </div>
   );
 
-  function Login(){
-      setLogin(true)
   }
-  function Logout(){
-    setLogin(false)
-  }
-  
-}
-
-
 
 
 
--- src/Components/Add.js
+++ src/Components/Add.js
@@ -1,14 +1,14 @@
-function Add({fname,age,place}) {
-    // console.log(props.data)
-    return (
-      <div>
-        {/* <h1>{JSON.stringify(props.data)}</h1> */}
-        <h1>{fname}</h1>
-        <h1>{age}</h1>
-        <h1>{place}</h1>
-      </div>
-    );
-  }
+// function Add({fname,age,place}) {
+//     // console.log(props.data)
+//     return (
+//       <div>
+//         {/* <h1>{JSON.stringify(props.data)}</h1> */}
+//         <h1>{fname}</h1>
+//         <h1>{age}</h1>
+//         <h1>{place}</h1>
+//       </div>
+//     );
+//   }
   
-  export default Add;
+//   export default Add;
   
